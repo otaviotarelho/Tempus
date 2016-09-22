26942ab15fd08326da51ef59bacc97e0c913eb44
Otavio Tarelho <otarelho@ucdavis.edu>
2016-09-21 22:00:46 -0300

    New alarm template - 50%

--- app/src/main/java/com/tempus/MainActivity.java
+++ app/src/main/java/com/tempus/MainActivity.java
@@ -16,8 +16,6 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/tempus/NewAlarmActivity.java
+++ app/src/main/java/com/tempus/NewAlarmActivity.java
@@ -5,15 +5,123 @@
 package com.tempus;
 
 /* Imports section */
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class NewAlarmActivity extends AppCompatActivity{
+public class NewAlarmActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_alarm);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new newAlarmItems()).commit();
     }
+
+    public static class newAlarmItems extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_new_alarm);
+            setHasOptionsMenu(true);
+
+            //First Category
+            bindPreferenceSummaryToValue(findPreference("alarm_time"));
+            bindPreferenceSummaryToValue(findPreference("alarm_name"));
+            bindPreferenceSummaryToValue(findPreference("alarm_repeat"));
+            bindPreferenceSummaryToValue(findPreference("alarm_ringtone"));
+            bindPreferenceSummaryToValue(findPreference("alarm_type"));
+
+            //Second Category
+            bindPreferenceSummaryToValue(findPreference("event_location"));
+            bindPreferenceSummaryToValue(findPreference("event_start_time"));
+            bindPreferenceSummaryToValue(findPreference("event_end_time"));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_alarm, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            if (!super.onMenuItemSelected(featureId, item)) {
+                NavUtils.navigateUpFromSameTask(this);
+            }
+            return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id == R.id.action_save_alarm) {
+            saveData();
+            Log.e("Save selected", "TRUE");
+        }
+
+        return super.onOptionsItemSelected(item);
+    } // end of onOptionItemSelected
+
+    public void saveData(){
+        //saveData in the database
+    }
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+
+            }
+            else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
 }
 
