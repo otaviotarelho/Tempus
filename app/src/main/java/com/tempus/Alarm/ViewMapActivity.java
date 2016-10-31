bf3a04e739b24d0712c13d9983f3812490f7da40
Diego Nunes Reis <diegonunesftw@gmail.com>
2016-10-31 21:24:53 -0200

    Some changes on ViewMapActivity
    It's not done yet. I'm still working on it.

--- app/src/main/java/com/tempus/Alarm/ViewMapActivity.java
+++ app/src/main/java/com/tempus/Alarm/ViewMapActivity.java
@@ -8,14 +8,28 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.tempus.R;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -26,19 +40,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import org.json.JSONObject;
 
-public class ViewMapActivity extends FragmentActivity implements OnMapReadyCallback, DistanceFinder.DF {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ViewMapActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, DistanceFinder.DF {
     private GoogleMap mMap;
     String currentPosition;
-    LatLng currentLat;
     String destination;
+    LatLng currentLat;
+    LatLng destLat;
 
-    String APIKEY = "AIzaSyDDTNXhkFDJKh5-em6fVXcDpT4Wp5aA7ZI";
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    LocationRequest mLocationRequest;
 
-    double longitude;
-    double latitude;
+    GoogleApiClient mGoogleApiClient;
+    //String APIKEY = "AIzaSyDDTNXhkFDJKh5-em6fVXcDpT4Wp5aA7ZI";
 
     LocationManager lm;
     Location location;
@@ -50,53 +78,112 @@ public class ViewMapActivity extends FragmentActivity implements OnMapReadyCallb
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_map);
 
-        // ---------------   pegando a posição atual  --------------------
         lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        //requisitando permissões
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
-        currentPosition = latitude + "," + longitude;
-        currentLat = new LatLng(latitude, longitude);
+        //if (location != null) {
+            currentLat = new LatLng(location.getLatitude(), location.getLongitude());
+        //}
 
-        // ---------------   pegando a posição de destino  --------------------
-        //Recebendo a string com as coordenadas, removendo sujeira e transformando em double pra entao transformar em LatLng
-        Intent intent = getIntent();
-        String destination = intent.getStringExtra("LOCATION");
-        String aux[] = destination.split(":");
-        destination = aux[1].substring(2, (aux[1].length() - 1));
-
-        Log.v("origem", String.valueOf(currentPosition));
-        Log.v("destino", String.valueOf(destination));
+        getDestinationPosition();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.viewMap);
         mapFragment.getMapAsync(this);
-
-        //agora eu vou só chamar a classe que calcula distancia, só pra tu ver
-        timeTextView = (TextView) findViewById(R.id.timeTextView);
-        distanceTextView = (TextView) findViewById(R.id.distanceTextView);
-
-        String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + currentPosition + "&destinations=" + destination + "&mode=driving&language=pt-BR&avoid=tolls&key=" + APIKEY;
-        new DistanceFinder(ViewMapActivity.this).execute(url);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //moving camera into the actual position
-        mMap.addMarker(new MarkerOptions().position(currentLat).title("Posição atual"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLat));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+
+        //criando a URL
+        String url = getUrl(currentLat, destLat);
+        Log.d("MAPS URL", url.toString());
+        //traçando a rota
+        FetchUrl FetchUrl = new FetchUrl();
+        FetchUrl.execute(url);
+
+        String DistanceFinderURL = getDistanceUrl(currentLat, destLat);
+
+        timeTextView = (TextView) findViewById(R.id.timeTextView);
+        distanceTextView = (TextView) findViewById(R.id.distanceTextView);
+
+        //Inserindo informações de rota
+        new DistanceFinder(ViewMapActivity.this).execute(DistanceFinderURL);
+
+        //marking the destination
+        mMap.addMarker(new MarkerOptions().position(destLat).title("Destino").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         mMap.setMyLocationEnabled(true);
+
+        //moving camera into the actual position
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLat));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    private String getUrl(LatLng origin, LatLng dest) {
+
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        String sensor = "sensor=false";
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+        return url;
+    }
+
+    private String getDistanceUrl(LatLng origin, LatLng dest){
+        String str_origin = "origins=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destinations=" + dest.latitude + "," + dest.longitude;
+
+        String output = "json";
+
+        String mode = "mode=driving";
+        String language = "language=pt-BR";
+        String avoid = "avoid=tolls";
+        String key = "key=AIzaSyDDTNXhkFDJKh5-em6fVXcDpT4Wp5aA7ZI";
+
+        String parameters = str_origin + "&" + str_dest + "&" + mode + "&" + language + "&" + avoid + "&" + key;
+
+        String url = "https://maps.googleapis.com/maps/api/distancematrix/" + output + "?" + parameters;
+        return url;
+    }
+
+
+    protected void getDestinationPosition() {
+        // ---------------   pegando a posição de destino  --------------------
+        //Recebendo a string com as coordenadas, removendo sujeira e transformando em double pra entao transformar em LatLng
+        Intent intent = getIntent();
+        destination = intent.getStringExtra("LOCATION");
+        String aux[] = destination.split(":");
+        destination = aux[1].substring(2, (aux[1].length() - 1));
+        String aux2[] = destination.split(",");
+        destLat = new LatLng(Double.valueOf(aux2[0]), Double.valueOf(aux2[1]));
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     @Override
@@ -106,6 +193,206 @@ public class ViewMapActivity extends FragmentActivity implements OnMapReadyCallb
         int distance = (Integer.parseInt(res[1]) / 1000);
         timeTextView.setText((int) (minutes / 60) + " horas e " + (int) (minutes % 60) + " min");
         distanceTextView.setText(distance + " km");
+    }
+
+    // Fetches data from url passed
+    private class FetchUrl extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try {
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+                Log.d("Background Task data", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+
+    /**
+     * A method to download json data from url
+     */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask", jsonData[0].toString());
+                DataParser parser = new DataParser();
+                Log.d("ParserTask", parser.toString());
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+                Log.d("ParserTask", "Executing routes");
+                Log.d("ParserTask", routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask", e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.RED);
+
+                Log.d("onPostExecute", "onPostExecute lineoptions decoded");
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            if (lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            } else {
+                Log.d("onPostExecute", "without Polylines drawn");
+            }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mLastLocation = location;
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+
+        //Place current location marker
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+        //move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
 }
